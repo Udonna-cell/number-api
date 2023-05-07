39241cde2e90486e45dde8aacadc092a6e9d2e1f
Stanley-Orabueze <67174967+Udonna-cell@users.noreply.github.com>
2023-05-07 18:27:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,30 +3,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
 
-
-
-
-
-var mysql = require('mysql');
-
-var con = mysql.createConnection({
-  //MYSQL_URL: "mysql://root:b96RjdxJC8Wi9aopnwhw@containers-us-west-51.railway.app:7134/railway",
-//MYSQLDATABASE: "railway",
-host: "containers-us-west-51.railway.app",
-//MYSQLPORT: 7134,
-user: "root",
-password: "b96RjdxJC8Wi9aopnwhw"
-});
-
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
-
-
-
-
-
 dotenv.config();
 
 const app = express();
